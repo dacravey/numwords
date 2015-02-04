da54be2759dc2182db3150073c2e5c423939fbe3
Debbie <cinemaestro@yahoo.com>
2015-02-03 18:22:47 -0800

    add larger hash and specs for all three digit numbers

--- spec/numword_spec.rb
+++ spec/numword_spec.rb
@@ -7,4 +7,16 @@ describe('numword') do
   it("takes a user entered single-digit number and returns it as written words") do
     expect(5.numword()).to eq("five")
   end
+
+  it('takes a teen-number and returns it in words')do
+    expect(14.numword()).to eq("fourteen")
+  end
+
+  it('takes a two-digit number and returns it in words')do
+    expect(27.numword()).to eq("twenty seven")
+  end
+
+  it('takes a three-digit number and returns it in words')do
+    expect(333.numword()).to eq ("three hundred thirty three")
+  end
 end
